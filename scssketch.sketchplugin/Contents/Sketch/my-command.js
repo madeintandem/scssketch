72964940fe2de1502be9eb799ab2c37ca4b8849f
Max Oppenheimer <max.oppenheimer@devmynd.com>
2018-08-30 15:59:33 -0500

    log object containing colors and shadows

--- scssketch.sketchplugin/Contents/Sketch/my-command.js
+++ scssketch.sketchplugin/Contents/Sketch/my-command.js
@@ -103,14 +103,23 @@ __webpack_require__.r(__webpack_exports__);
 var sketch = __webpack_require__(/*! sketch */ "sketch");
 
 /* harmony default export */ __webpack_exports__["default"] = (function (context) {
-  // const document = sketch.toJSON(context.document)
-  var Documents = sketch.getDocument();
-  context.document.showMessage(documents); // Document.open("document.json", (err, document) => {
-  //   debugger
-  //   if (err) {
-  //   } else {
-  //   }
-  // })
+  var document = sketch.getSelectedDocument();
+  var sharedStyles = document.sketchObject.documentData().layerStyles();
+
+  function separateColorAndShadow(style) {
+    if (String(style.name()).charAt(0) == '[') {
+      context.document.showMessage("color");
+    } else {
+      context.document.showMessage("shadow");
+    }
+  }
+
+  for (var i = 0; i < sharedStyles.numberOfSharedStyles(); i++) {
+    layerStyle = sharedStyles.objects().objectAtIndex(i);
+    separateColorAndShadow(layerStyle); //var colorName = String(layerStyle.name());
+    //var colorHex = "#" + layerStyle.value().firstEnabledFill().color().immutableModelObject().hexValue();
+    //console.log(colorName + " " + colorHex)
+  }
 });
 
 /***/ }),
--- src/my-command.js
+++ src/my-command.js
@@ -1,25 +1,43 @@
-const sketch = require('sketch')
-
 export default function(context) {
+  var sketch = require('sketch')
   var document = sketch.getSelectedDocument()
 
   var sharedStyles = document.sketchObject.documentData().layerStyles()
 
+  var scss = {
+    colors: [],
+    shadows: []
+  }
   function separateColorAndShadow(style) {
     if (String(style.name()).charAt(0) == '[') {
-      console.log("color")
+      addColor(style)
     } else {
-      console.log("shadow")
+      addShadow(style)
     }
+  }
+
+  function addColor(style) {
+    let name = String(style.name()).split(' ').pop().concat('_color')
+    let hex = "#" + layerStyle.value().firstEnabledFill().color().immutableModelObject().hexValue()
+    var tmp = {}
+    tmp[name] = hex
+    scss.colors.push(tmp)
+  }
+
+  function addShadow(style) {
+    let name = String(style.name()).replace(' ', '_')
+    let hex = "#" + layerStyle.value().firstEnabledFill().color().immutableModelObject().hexValue()
+    var tmp = {}
+    tmp[name] = hex
+    scss.shadows.push(tmp)
   }
 
   for (var i = 0; i < sharedStyles.numberOfSharedStyles(); i++) {
 
     layerStyle = sharedStyles.objects().objectAtIndex(i);
     separateColorAndShadow(layerStyle)
-    //var colorName = String(layerStyle.name());
-    //var colorHex = "#" + layerStyle.value().firstEnabledFill().color().immutableModelObject().hexValue();
-    //console.log(colorName + " " + colorHex)
+  }
+
+  console.log(scss)
   }
 }
-
